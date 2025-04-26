b44754e1a1932352e9d6082f6b1950d7e98a8949
MreRes <47110975+MreRes@users.noreply.github.com>
2025-04-27 05:40:18 +0700

    Add backend/src/utils/verifyEnv.js 1745707220272

---
+++ backend/src/utils/verifyEnv.js
@@ -0,0 +1,4 @@
+console.log('JWT_SECRET:', process.env.JWT_SECRET ? 'Loaded' : 'Not Loaded');
+console.log('JWT_REFRESH_SECRET:', process.env.JWT_REFRESH_SECRET ? 'Loaded' : 'Not Loaded');
+console.log('SESSION_SECRET:', process.env.SESSION_SECRET ? 'Loaded' : 'Not Loaded');
+console.log('ENCRYPTION_KEY:', process.env.ENCRYPTION_KEY ? 'Loaded' : 'Not Loaded');
